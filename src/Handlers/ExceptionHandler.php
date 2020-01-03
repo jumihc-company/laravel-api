eeb2319ea9b6896791e818743aa346f35bd3d855
yl <1355470410@qq.com>
2020-01-03 12:02:50 +0800

    chore: sms.exception 格式修改

--- src/Handlers/ExceptionHandler.php
+++ src/Handlers/ExceptionHandler.php
@@ -134,7 +134,7 @@ class ExceptionHandler extends Handler
             // easySms 短信异常
             Log::save(
                 config('jmhc-api.sms_exception_file_name', 'sms.exception'),
-                $e->getMessage() . PHP_EOL . $e->getTraceAsString() . PHP_EOL . json_encode($e->getExceptions(), JSON_UNESCAPED_UNICODE)
+                json_encode($e->getExceptions(), JSON_UNESCAPED_UNICODE) . PHP_EOL . $e->getMessage() . PHP_EOL . $e->getTraceAsString()
             );
         } elseif ($e instanceof SmsException) {
             // 短信异常
